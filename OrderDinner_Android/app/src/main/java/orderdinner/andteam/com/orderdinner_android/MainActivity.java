b61eed4e15346c3c442a2f979b75380361df4ed4
陆鑫 <ex-luxin@pingan.com.cn>
2015-09-07 19:24:41 +0800

    test Git
    test Git push

--- OrderDinner_Android/app/src/main/java/orderdinner/andteam/com/orderdinner_android/MainActivity.java
+++ OrderDinner_Android/app/src/main/java/orderdinner/andteam/com/orderdinner_android/MainActivity.java
@@ -1,9 +1,15 @@
 package orderdinner.andteam.com.orderdinner_android;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by EX-BAIXUE600 on 2015/9/7.
  */
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
