a50e39ccd7f4901626b715c5a3d7cd9b60cbdc3b
baixue <EX-BAIXUE600@pingan.com.cn>
2015-09-07 19:21:17 +0800

    create Activity

---
+++ OrderDinner_Android/app/src/main/java/orderdinner/andteam/com/orderdinner_android/MainActivity.java
@@ -0,0 +1,9 @@
+package orderdinner.andteam.com.orderdinner_android;
+
+import android.app.Activity;
+
+/**
+ * Created by EX-BAIXUE600 on 2015/9/7.
+ */
+public class MainActivity extends Activity {
+}
---
+++ OrderDinner_Android/app/src/main/java/orderdinner/andteam/com/orderdinner_android/MyApplication.java
@@ -0,0 +1,9 @@
+package orderdinner.andteam.com.orderdinner_android;
+
+import android.app.Application;
+
+/**
+ * Created by EX-BAIXUE600 on 2015/9/7.
+ */
+public class MyApplication extends Application{
+}
